60e5925f34586a0f08b146ef286c8f8650a55dc2
Nuno Araújo <nuno.araujo@itsector.pt>
2024-09-26 19:37:43 +0100

    updated: chat app client

---
+++ src/Application/ChatApp.Client/Hubs/Chat/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Client.Hubs.Chat;
+
+public class ChatHub : Hub
+{
+    public Task SendMessageToAll(string user, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
---
+++ src/Application/ChatApp.Client/Hubs/Messages/MessageHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Client.Hubs.Messages;
+
+public class MessageHub : Hub
+{
+    public Task SendMessageToAll(string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
