3792402e1fa65745c89fcce4ca7afdcb94a225fa
Nuno Araújo <nuno.araujo@itsector.pt>
2024-09-26 19:16:53 +0100

    Added: client app

---
+++ src/Application/ChatApp.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ChatApp.Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
