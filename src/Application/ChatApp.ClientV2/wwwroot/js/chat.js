9af4feb8c9bd213c822ac3a8e8975247cedcb6dd
Nuno Araújo <nuno.araujo@itsector.pt>
2024-09-26 20:09:22 +0100

    removed: unwanted projects

--- src/Application/ChatApp.ClientV2/wwwroot/js/chat.js
+++ src/Application/ChatApp.ClientV2/wwwroot/js/chat.js
@@ -7,10 +7,8 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
+
     document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you
-    // should be aware of possible script injection concerns.
     li.textContent = `${user} says: ${message}`;
 });
 
@@ -23,10 +21,11 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
+
+    document.getElementById("messageInput").value = "";
     event.preventDefault();
-}).then(() => {
-    document.getElementById("userInput").innerHTML = "";
 });
