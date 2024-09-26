07af25cd9493635d2ab667e77028f65db29fc5b8
Nuno Araújo <nuno.araujo@itsector.pt>
2024-09-26 19:58:52 +0100

    updated: working client side app

---
+++ src/Application/ChatApp.ClientV2/Hubs/Chat/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.ClientV2.Hubs.Chat;
+
+//A hub is a class that serves as a high-level pipeline that handles client-server communication
+//The Hub class manages connections, groups, and messaging
+public class ChatHub : Hub
+{
+    /// <summary>
+    /// method can be called by a connected client to send a message to all clients
+    /// </summary>
+    /// <param name="user"></param>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
