d3f31def648bd5ebd51e3ebfd5318692a8476a43
SilverX21 <nuno21araujo95@gmail.com>
2024-09-30 10:59:27 +0100

    updated: validations

--- src/Domain/ChatApp.Domain/Validators/MessageValidator/MessageValidator.cs
+++ src/Domain/ChatApp.Domain/Validators/MessageValidator/MessageValidator.cs
@@ -8,7 +8,7 @@ public class MessageValidator : AbstractValidator<MessageInputModel>
     public MessageValidator()
     {
         RuleFor(message => message).NotNull().WithMessage("The input cannot be null, please provide a valid message.");
-        //RuleFor(message => message.UserId).NotNull().WithMessage("The message must be associated to a User.");
+        RuleFor(message => message.UserId).NotNull().WithMessage("The message must be associated to a User.");
         RuleFor(message => message.Content).NotNull().NotEmpty().WithMessage("The content of the message cannot be null, please provide a valid message.");
         RuleFor(message => message.Content).MaximumLength(1000).WithMessage("The content of the message exceeds the max of 1000 characters.");
     }
